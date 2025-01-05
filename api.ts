83f50a4e0c1f4a307f87a9d59d8845a3db429b17
wesley gado <wesleygado@gmail.com>
2025-01-05 18:10:29 -0300

    instalando json-server

---
+++ api.ts
@@ -0,0 +1,15 @@
+async function getAllTasks() {
+  // Fetch all tasks from the database
+}
+
+async function createTask() {
+  // Create a new task in the database
+}
+
+async function updateTask() {
+  // Update a task in the database
+}
+
+async function deleteTask() {
+  // Delete a task from the database
+}
